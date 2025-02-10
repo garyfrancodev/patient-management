4f9f9c61ad83412a1199ae77130d39df3b3dd2fb
Gary Franco <gary.franco.cespedes@gmail.com>
2025-02-09 21:06:08 -0400

    feat(): added pact to consumers and providers contract

---
+++ src/Person.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace src;
+
+class Person
+{
+    private $firstName;
+    private $lastName;
+    private $alias;
+
+    public function __construct(
+        string $firstName,
+        string $lastName,
+        string $alias
+    ) {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->alias = $alias;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function getAlias(): string
+    {
+        return $this->alias;
+    }
+}
