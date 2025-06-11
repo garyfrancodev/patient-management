54a1e8bdf2947950be64d890c47f1b68cf058e04
Gary Franco <gary.franco.cespedes@gmail.com>
2025-06-11 15:47:39 -0400

    feat(): added outbox, topics and update config

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Infrastructure\Providers\AppServiceProvider::class,
     App\Infrastructure\Providers\CommandBusServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
